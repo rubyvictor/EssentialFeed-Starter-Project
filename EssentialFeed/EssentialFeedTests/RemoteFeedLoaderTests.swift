10e354030654be105991a108fef5ca71e18da0fd
Victor Lee <myleevictor@gmail.com>
2025-08-14 14:20:07 +0800

    Corrected test method name to reflect test

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -22,7 +22,7 @@ class HTTPClient {
 
 class RemoteFeedLoaderTests: XCTestCase {
     
-    func test_init() {
+    func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClient.shared
         _ = RemoteFeedLoader()
         

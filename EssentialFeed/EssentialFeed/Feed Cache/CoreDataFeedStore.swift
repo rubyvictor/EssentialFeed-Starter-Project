09ec2fb6107c8abe8bbf77400c658a74edafd9fa
Victor Lee <myleevictor@gmail.com>
2025-09-20 17:38:09 +0800

    Rectify missing property in model representation of ManagedFeedImage

--- EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -37,6 +37,7 @@ private class ManagedFeedImage: NSManagedObject {
     @NSManaged var id: UUID
     @NSManaged var imageDescription: String?
     @NSManaged var location: String?
+    @NSManaged var url: URL
     @NSManaged var cache: ManagedCache
 }
 
